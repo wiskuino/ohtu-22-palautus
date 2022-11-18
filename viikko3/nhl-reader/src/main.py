34a6a12dff046cf42208e870456485113d70812c
Esko Hakala <esko.hakala@hotmail.com>
2022-11-18 20:11:42 +0200

    1. tehtava valmis

---
+++ viikko3/nhl-reader/src/main.py
@@ -0,0 +1,47 @@
+import requests
+from player import Player
+from datetime import datetime
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],player_dict['team'],player_dict['goals'],player_dict['assists']
+        )
+        if player_dict['nationality'] == "FIN":
+            players.append(player)
+
+    print(f"Players from FIN {datetime.now()}")
+    print()
+
+    for player in players:
+        print(player)
+
+if __name__=="__main__":
+    main()
+
+ 
+
+ 
+ 
+
+ 
+
+ 
+ 
+ 
+ 
+
+ 
+
+ 
+
+ 
+ 
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, goals , assists):
         self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}" 
