187102f15ad7c8d1d94cbc98a18268f0134bfd1d
Esko Hakala <esko.hakala@hotmail.com>
2022-11-25 17:17:54 +0200

    siis nyt ostoskorissa ei ole tuoetteita, testataan paluuarvoa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -8,4 +8,4 @@ class TestOstoskori(unittest.TestCase):
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
-        self.assertEqual(self.kori.tavaroita_korissa(),1)
+        self.assertEqual(self.kori.tavaroita_korissa(),0)
