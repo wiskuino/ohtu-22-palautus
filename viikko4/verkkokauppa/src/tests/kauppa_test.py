3ffd6f5b768d3e5613cefd4ecb280b34cd930885
Esko Hakala <esko.hakala@hotmail.com>
2022-11-25 11:07:23 +0200

    tehtävä 4 kaikki testit mukana

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -21,13 +21,17 @@ class TestKauppa(unittest.TestCase):
                 return 10
             if tuote_id == 2:
                 return 20
-
+            if tuote_id == 3:
+                return 0
+            
         # tehdään toteutus hae_tuote-metodille
         def varasto_hae_tuote(tuote_id):
             if tuote_id == 1:
                 return Tuote(1, "maito", 5)
             if tuote_id == 2:
                 return Tuote(2, "nakit", 10)
+            if tuote_id == 3:
+                return Tuote(3, "olut", 20)
 
         # otetaan toteutukset käyttöön
         varasto_mock.saldo.side_effect = varasto_saldo
@@ -123,7 +127,7 @@ class TestKauppa(unittest.TestCase):
         self.pankki_mock.tilisiirto.assert_called_with("pekka",42,"12345","33333-44455",15)
         # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
 
-    def test_kahden_saman_ostoksen_paatyttya_pankin_metodia_tilisiirto_kutsutaan_oikeilla_parametreilla(self):
+    def test_2_saman_ostoksen_paatyttya_pankin_metodia_tilisiirto_kutsutaan_oikeilla_parametreilla(self):
         # tehdään ostokset
         self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(2)
@@ -132,4 +136,14 @@ class TestKauppa(unittest.TestCase):
 
         # varmistetaan, että metodia tilisiirto on kutsuttu
         self.pankki_mock.tilisiirto.assert_called_with("pekka",42,"12345","33333-44455",20)
+        # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+
+    def test_2_ostoksen_paatyttya_toisen_saldo_0_pankin_metodia_tilisiirto_kutsutaan_oikeilla_parametreilla(self):
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.tilimaksu("pekka", "12345")
+        # varmistetaan, että metodia tilisiirto on kutsuttu
+        self.pankki_mock.tilisiirto.assert_called_with("pekka",42,"12345","33333-44455",10)
         # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
