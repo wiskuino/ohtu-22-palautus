07d77ad2a417134cc0f9c2ebb1928902d09d2ae3
Esko Hakala <esko.hakala@hotmail.com>
2022-11-26 10:29:45 +0200

    Test5:ostoskorin hinta oikein 2 eri tuotetta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -25,7 +25,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.juusto)
         self.assertEqual(self.kori.tavaroita_korissa(),2)
     
-
+    def test_toisen_tuotteen_lisaamisen_jalkeen_korin_hinta_oikein(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.juusto)
+        self.assertEqual(self.kori.hinta(),7)
 
 
 
