ffba5f57cf15463cf3f1697ad93b320a8dd642db
Esko Hakala <esko.hakala@hotmail.com>
2022-11-30 14:04:00 +0200

    Siirretty koodia luotuun uuteen metodiin advantage_case

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,62 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
+        self.score = ""
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_score = self.player1_score + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score = self.player2_score + 1
 
     def get_score(self):
-        score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
+        if self.player1_score == self.player2_score:
+            if self.player1_score == 0:
+                self.score = "Love-All"
+            elif self.player1_score == 1:
+                self.score = "Fifteen-All"
+            elif self.player1_score == 2:
+                self.score = "Thirty-All"
+            elif self.player1_score == 3:
+                self.score = "Forty-All"
             else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+                self.score = "Deuce"
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            self.advantage_case(self.score,self.player1_score,self.player2_score)  
+        
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1_score
                 else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+                    self.score = self.score + "-"
+                    temp_score = self.player2_score
 
                 if temp_score == 0:
-                    score = score + "Love"
+                    self.score = self.score + "Love"
                 elif temp_score == 1:
-                    score = score + "Fifteen"
+                    self.score = self.score + "Fifteen"
                 elif temp_score == 2:
-                    score = score + "Thirty"
+                    self.score = self.score + "Thirty"
                 elif temp_score == 3:
-                    score = score + "Forty"
+                    self.score = self.score + "Forty"
 
-        return score
+    
+        return self.score
+
+    def advantage_case(self, score, player1_score, player2_score):
+        minus_result = self.player1_score - self. player2_score
+        if minus_result == 1:
+            self.score = "Advantage player1"
+        elif minus_result == -1:
+            self.score = "Advantage player2"
+        elif minus_result >= 2:
+            self.score = "Win for player1"
+        else:
+            self.score = "Win for player2"
+        return self.score
