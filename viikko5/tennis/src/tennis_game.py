5cdb14af774984e1057ebaffa988f74875451228
Esko Hakala <esko.hakala@hotmail.com>
2022-11-30 17:36:26 +0200

    Turha attrubuutti temp_score luokasta hittoon

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -5,7 +5,6 @@ class TennisGame:
         self.player1_score = 0
         self.player2_score = 0
         self.score = ""
-        self.temp_score = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
@@ -19,10 +18,10 @@ class TennisGame:
             self.score = self.deuce_case()
 
         elif self.player1_score >= 4 and self.player1_score > self.player2_score:
-            self.score = self.winning_player_1()  # player1 johtaa
+            self.score = self.winning_player_1()  # player_1 johtaa
         
         elif self.player2_score >= 4 and self.player2_score > self.player1_score:
-            self.score = self.winning_player_2()   # player2 johtaa
+            self.score = self.winning_player_2()   # player_2 johtaa
         
         else:
             self.score = self.playing_case()
@@ -30,40 +29,22 @@ class TennisGame:
         return self.score
 
     def playing_case(self):
-        scores = {0:"Love",1:"Fifteen",2:"Thirty",3:"Forty"}
+        scores = {0:"Love",1:"Fifteen",2:"Thirty",3:"Forty"}  # sanakirjaa tarvitaan vain metodissa
         return f"{scores[self.player1_score]}-{scores[self.player2_score]}"
-        
-        
-        
-        #for i in range(1, 3):
-            #if i == 1:
-            #    self.temp_score = self.player1_score
-            #else:
-            #    self.score = self.score + "-"
-            #    self.temp_score = self.player2_score
-            #if self.temp_score == 0:
-            #    self.score = self.score + "Love"
-            #elif self.temp_score == 1:
-            #    self.score = self.score + "Fifteen"
-            #elif self.temp_score == 2:
-            #    self.score = self.score + "Thirty"
-            #elif self.temp_score == 3:
-            #    self.score = self.score + "Forty"
-        #return self.score
 
     def winning_player_1(self):
-        scores = {1:"Advantage player1",2:"Win for player1",3:"Win for player1", 4:"Win for player1"}
+        scores = {1:"Advantage player1",2:"Win for player1",3:"Win for player1", 4:"Win for player1"} # sanakirjaa tarvitaan vain metodissa
         return scores[self.player1_score - self. player2_score]  
         
 
     def winning_player_2(self):
-        scores = {1:"Advantage player2",2:"Win for player2",3:"Win for player2",4:"Win for player2"}
+        scores = {1:"Advantage player2",2:"Win for player2",3:"Win for player2",4:"Win for player2"} # sanakirjaa tarvitaan vain metodissa
         return scores[self.player2_score - self. player1_score]
         
     def deuce_case(self):
         if self.player1_score > 3:
             return "Deuce"
         else:
-            scores = {0:"Love-All",1:"Fifteen-All",2:"Thirty-All",3:"Forty-All"}
+            scores = {0:"Love-All",1:"Fifteen-All",2:"Thirty-All",3:"Forty-All"} # sanakirjaa tarvitaan vain metodissa
             return scores[self.player1_score]
 
