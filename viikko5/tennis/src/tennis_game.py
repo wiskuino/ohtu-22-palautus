df549191420c544b6c95820613a9a1eeaed0275e
Esko Hakala <esko.hakala@hotmail.com>
2022-12-04 19:01:32 +0200

    Vielä korjailtiin metodien nimiä

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -4,7 +4,7 @@ class TennisGame:
         self.player_2 = name_2
         self.player_1_points = 0
         self.player_2_points = 0
-        self.scoring_call = {0:"Love",1:"Fifteen",2:"Thirty",3:"Forty"}
+        self.scoring_steps = {0:"Love",1:"Fifteen",2:"Thirty",3:"Forty"}
 
     def won_point(self, player):
         if player == self.player_1:
@@ -16,24 +16,24 @@ class TennisGame:
 
         if (self.player_1_points >= 4 or self.player_2_points >= 4) \
             and self.player_1_points != self.player_2_points:
-            leader = self.player_1 if self.player_1_points > self.player_2_points \
-                else self.player_2
-            
-            return self.player_x_advantage(leader,abs(self.player_1_points - self. player_2_points)) 
-
+            return self.almost_points_enough()
         else:
-            return self.continue_playing()
+            return self.collect_more_points()
+    
+    def almost_points_enough(self):
+        leader = self.player_1 if self.player_1_points > self.player_2_points else self.player_2
+        return self.player_x_advantage(leader,abs(self.player_1_points - self. player_2_points)) 
 
     def player_x_advantage(self,leader,leader_points): 
         cases = {1:"Advantage "+leader,2:"Win for "+leader,3:"Win for "+leader, 4:"Win for "+leader} 
         return cases[leader_points]
 
-    def deuce(self):
-        return "Deuce" if self.player_1_points > 3 else f"{self.scoring_call[self.player_1_points]}-All" 
+    def deuce_or_All(self):
+        return "Deuce" if self.player_1_points > 3 else f"{self.scoring_steps[self.player_1_points]}-All" 
     
-    def continue_playing(self):
-        return f"{self.scoring_call[self.player_1_points]}-{self.scoring_call[self.player_2_points]}" \
-             if self.player_2_points != self.player_1_points else self.deuce()
+    def collect_more_points(self):
+        return f"{self.scoring_steps[self.player_1_points]}-{self.scoring_steps[self.player_2_points]}" \
+             if self.player_2_points != self.player_1_points else self.deuce_or_All()
             
             
 
