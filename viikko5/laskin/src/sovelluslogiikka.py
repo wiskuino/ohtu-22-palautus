0e9eb8f95829342e8256a25ef8c971df3a557618
Esko Hakala <esko.hakala@hotmail.com>
2022-12-06 14:15:54 +0200

    Lisättiin KUMOAn vaatima lista, eli tässä voi palauttaa tilanteen vaikka kuinka pitkälti

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -3,18 +3,24 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.edelliset = []
 
     def miinus(self, arvo):
+        self.edelliset.append(self.tulos)
         self.tulos = self.tulos - arvo
+        
 
     def plus(self, arvo):
+        self.edelliset.append(self.tulos)
         self.tulos = self.tulos + arvo
-
+        
+    
     def nollaa(self):
+        self.edelliset.append(self.tulos)
         self.tulos = 0
 
-    def kumoa(self,edellinen):
-        self.tulos=edellinen
+    def kumoa(self):
+        self.tulos=self.edelliset.pop(-1)
 
     def aseta_arvo(self, arvo):
         self.tulos = arvo
