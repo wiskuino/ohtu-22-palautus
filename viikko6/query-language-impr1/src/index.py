99ce9185e2c542606d4212235d8fb707d9e0cb07
Esko Hakala <esko.hakala@hotmail.com>
2022-12-09 17:44:58 +0200

    Korjattiin playsIn metodi ja lisättiin hasFewerThan ja hasAtLest metodit

--- viikko6/query-language-impr1/src/index.py
+++ viikko6/query-language-impr1/src/index.py
@@ -10,64 +10,17 @@ def main():
     stats = Statistics(reader)
 
     query = QueryBuilder()
-    matcher= query.playsIn("NYR").build()
 
-    for player in stats.matches(matcher):
+    print("välirivi")
+
+    matcher2 = (
+        query
+        .hasAtLeast(10, "goals")
+        .hasFewerThan(20, "goals")
+        .playsIn("NYR").build())
+
+    for player in stats.matches(matcher2):
         print(player)
-
-
-
-
-   #matcher = And(
-   #   HasAtLeast(5, "goals"),
-   #   HasAtLeast(5, "assists"),
-   #   PlaysIn("PHI"))
-   #   
-   #for player in stats.matches(matcher):
-   #   print(player)
-
-    #matcher2 = All( "name")
-#
-    #for player in stats.matches(matcher2):
-    #   print(player)
-
-    #matcher3 = Not(HasAtLeast(1,"goals"))    
-    #matcher3 = And(
-    #    Not(HasAtLeast(1, "goals")),
-    #    PlaysIn("NYR"))   
-    
-   #matcher3 = Or(
-   #HasAtLeast(45, "goals"),
-   #HasAtLeast(70, "assists"))
-   #
-   #
-   #
-
-   #
-   ##
-
-   #matcher = And(
-   #HasFewerThan(1, "goals"),
-   #PlaysIn("NYR"))
-
-    #matcher = And(
-    #HasAtLeast(70, "points"),
-    #Or(
-    #    PlaysIn("NYR"),
-    #    PlaysIn("FLA"),
-    #    PlaysIn("BOS")))
-#
-    #for player in stats.matches(matcher):
-    #    print(player)
-    #print("välirivi")
-    #
-    #for player in stats.matches(matcher3):
-    #   print(player)
-    
-    
-
-
-
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language-impr1/src/query_builder.py
+++ viikko6/query-language-impr1/src/query_builder.py
@@ -1,4 +1,4 @@
-from matchers import All, PlaysIn
+from matchers import And, All, PlaysIn, HasAtLeast, HasFewerThan
 
 
 class QueryBuilder:
@@ -8,6 +8,19 @@ class QueryBuilder:
     def build(self):
         return self.matcher
     
-    def playsIn(self, team):
-        return QueryBuilder(self.matcher and PlaysIn(team))
+    def playsIn(self,team):
+        self.team =team
+        return QueryBuilder(And(self.matcher, PlaysIn(self.team)))
+
+    def hasAtLeast(self, value, attr):
+        self.value = value
+        self.attr = attr
+        return QueryBuilder(And(self.matcher, HasAtLeast(self.value,self.attr)))
+    
+    def hasFewerThan(self,value,attr):
+        self.value = value
+        self.attr = attr
+        return (QueryBuilder(And(self.matcher, HasFewerThan(self.value,self.attr))))
+
+
             
