65f9c80637299fd0bca09f28129409542d543e16
Esko Hakala <esko.hakala@hotmail.com>
2022-12-12 17:18:46 +0200

    tekoalyssa otettu käyttöön siirto-oliot jotka luodaan Siirto luokan konstruktorissa. Tekoälyn vastuulla on tietää mikä siirto on vuorossa. Siirto vuoroja ylläpidetään listassa, joka puolestaan initialisoidaan Tekoäly luokan konstrukstorissa. Tällä muutoksella poistui ikävän näköinen if - else tikapuu tekoäly luokan koodista.

--- viikko7/kivi-paperi-sakset/src/tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/tekoaly.py
@@ -1,18 +1,27 @@
+from enum import Enum
+
+
+class Siirto(Enum):
+    Kivi = "k"
+    Paperi = "p"
+    Sakset = "s"
+
+
 class Tekoaly:
     def __init__(self):
-        self._siirto = 0
+        self._siirto=0
+        self.siirrot = [Siirto.Kivi,Siirto.Paperi,Siirto.Sakset]
+
 
     def anna_siirto(self):
-        self._siirto = self._siirto + 1
+        self._siirto += 1
         self._siirto = self._siirto % 3
-
-        if self._siirto == 0:
-            return "k"
-        elif self._siirto == 1:
-            return "p"
-        else:
-            return "s"
+        return self.siirrot[self._siirto].value
 
     def aseta_siirto(self, siirto):
         # ei tehdä mitään
         pass
+
+
+
+
