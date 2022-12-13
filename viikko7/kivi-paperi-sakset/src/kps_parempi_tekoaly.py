72e315d7b3a08c40c932a5f62ff253eb5180d99f
Esko Hakala <esko.hakala@hotmail.com>
2022-12-13 12:11:37 +0200

    Tehtiin samat muutokset myös kahteen muuhun eli KPSParempi.. ja KPSPelaaja.. . Näissä kuten myös edellissä kommitissa refaktoroitu KPSTeko... muutoksessa sai Tuomari luokka vastuulleen myös _onko_ok_siirto metodin

--- viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -3,27 +3,26 @@ from tekoaly_parannettu import TekoalyParannettu
 
 
 class KPSParempiTekoaly:
+    def __init__(self):
+  
+        self.ekan_siirto= ""
+        self.tokan_siirto= ""
+    
     def pelaa(self):
         tuomari = Tuomari()
         tekoaly = TekoalyParannettu(10)
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
+        self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        self.tokan_siirto = tekoaly.anna_siirto()
 
-        print(f"Tietokone valitsi: {tokan_siirto}")
+        print(f"Tietokone valitsi: {self.tokan_siirto}")
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
+        while tuomari.tarkasta_siirrot(self.ekan_siirto,self.tokan_siirto):
 
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
+            self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+            self.tokan_siirto = tekoaly.anna_siirto()
 
-            print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
+            print(f"Tietokone valitsi: {self.tokan_siirto}")
+            tekoaly.aseta_siirto(self.ekan_siirto)
 
-        print("Kiitos!")
-        print(tuomari)
 
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
--- viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -2,21 +2,21 @@ from tuomari import Tuomari
 
 
 class KPSPelaajaVsPelaaja:
+    def __init__(self):
+     
+        self.ekan_siirto= ""
+        self.tokan_siirto= ""
+    
     def pelaa(self):
+        
+        
         tuomari = Tuomari()
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = input("Toisen pelaajan siirto: ")
+        self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        self.tokan_siirto = input("Toisen pelaajan siirto: ")
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
+        while tuomari.tarkasta_siirrot(self.ekan_siirto,self.tokan_siirto):
 
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = input("Toisen pelaajan siirto: ")
+            self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+            self.tokan_siirto = input("Toisen pelaajan siirto: ")
 
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
