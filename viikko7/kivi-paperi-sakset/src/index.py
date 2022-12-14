2f295bb116957f551e2d65c9422d0085289aeba0
Esko Hakala <esko.hakala@hotmail.com>
2022-12-14 17:36:01 +0200

    Luokka Pelitehdas toteutettu ja sijoitettu index.py tiedostoon circular reference ongemien vuoksi. Pelitehdas palauttaa valitun peli olion

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -3,7 +3,26 @@ from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
 
 
+
+class KPSPeliTehdas:
+    def __init__(self):
+        pelaaja_vastaan_pelaaja = KPSPelaajaVsPelaaja()
+        pelaaja_vastaan_tekoaly = KPSTekoaly()
+        pelaaja_vastaan_parempi_tekoaly = KPSParempiTekoaly()
+        
+        self.pelit = {"a":pelaaja_vastaan_pelaaja,"b":pelaaja_vastaan_tekoaly,"c":pelaaja_vastaan_parempi_tekoaly}
+    
+    
+    
+    def hae_peli(self,peli_valinta):
+        if peli_valinta in self.pelit:
+            return self.pelit[peli_valinta]
+        else:
+            exit
+
 def main():
+    pelitehdas = KPSPeliTehdas()
+    
     while True:
         print("Valitse pelataanko"
               "\n (a) Ihmistä vastaan"
@@ -12,32 +31,29 @@ def main():
               "\nMuilla valinnoilla lopetetaan"
               )
 
-        vastaus = input()
-
-        if vastaus.endswith("a"):
+        peli_valinta = input()
+        if peli_valinta.endswith("a") or peli_valinta.endswith("b") or peli_valinta.endswith("c"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+            peli = pelitehdas.hae_peli(peli_valinta)
+            peli.pelaa()
         else:
             break
 
 
+
+
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
