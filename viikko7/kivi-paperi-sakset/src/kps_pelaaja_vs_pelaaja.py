c747054a1e77bd764f1ee420b4268a912d952e28
Esko Hakala <esko.hakala@hotmail.com>
2022-12-14 15:13:24 +0200

    Muutettiin lukan KPSPelaajaVSPelaaja rakennata niin, että voidaan käyttää sitä yliluokkana, jolta muut pelityypit perivät geneerisen osan eli pelilogiikan ja pelaajien siirto metodit

--- viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -1,22 +1,29 @@
 from tuomari import Tuomari
 
-
-class KPSPelaajaVsPelaaja:
+class KPSPelaajaVsPelaaja():
     def __init__(self):
-     
-        self.ekan_siirto= ""
-        self.tokan_siirto= ""
+    
+        self.ekan_siirto = None
+        self.tokan_siirto = None
     
     def pelaa(self):
         
         
         tuomari = Tuomari()
 
-        self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        self.tokan_siirto = input("Toisen pelaajan siirto: ")
+        self.ekan_siirto = self.hae_ekan_siirto()
+        self.tokan_siirto = self.hae_tokan_siirto()
 
         while tuomari.tarkasta_siirrot(self.ekan_siirto,self.tokan_siirto):
 
-            self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            self.tokan_siirto = input("Toisen pelaajan siirto: ")
+            self.ekan_siirto = self.hae_ekan_siirto()
+            self.tokan_siirto = self.hae_tokan_siirto()
 
+    
+    def hae_ekan_siirto(self):
+        return input("Ensimmäisen pelaajan siirto: ")
+
+    
+    def hae_tokan_siirto(self):
+        return input("Toisen pelaajan siirto: ")
+        
