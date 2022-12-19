1fab592eedb8757e351f01ad8b6acad773e23c54
Esko Hakala <esko.hakala@hotmail.com>
2022-12-19 13:11:08 +0200

    paranettiin KPSTekoaly ja KPSParempiTekoaly peritymistä, luokissa erona vain riippuvuudet Tekoaly ja TekoalyParannettu

--- viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -6,20 +6,3 @@ from kps_tekoaly import KPSTekoaly
 class KPSParempiTekoaly(KPSTekoaly):
     def __init__(self):
         self.tekoaly = TekoalyParannettu(10)
-       
-    def hae_ekan_siirto(self):
-        self.ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        return self.ekan_siirto
-
-    def hae_tokan_siirto(self):
-        self.tokan_alykas_siirto = self.tekoaly.anna_siirto()
-        self.kerro_tietokoneen_valinta()
-        self.talleta_ekan_siirto(self.ekan_siirto)
-        return self.tokan_alykas_siirto
-
-    def kerro_tietokoneen_valinta(self):
-        print(f"Tietokone valitsi: {self.tokan_alykas_siirto}")
-    
-        
-    def talleta_ekan_siirto(self,siirto):
-        self.tekoaly.aseta_siirto(siirto)
--- viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
@@ -1,4 +1,4 @@
-from tuomari import Tuomari
+
 from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
 from tekoaly import Tekoaly
 
@@ -9,19 +9,23 @@ class KPSTekoaly(KPSPelaajaVsPelaaja):
         super().__init__()
         self.tekoaly =Tekoaly()
         
-        self.tokan_edellinen_siirto = self.tekoaly.siirrot[0]
         
     
     def hae_ekan_siirto(self):
-        return input("Ensimmäisen pelaajan siirto: ")
+        self.ekan_siirto =input("Ensimmäisen pelaajan siirto: ")
+        return self.ekan_siirto
         
     def hae_tokan_siirto(self):
         self.tokan_alykas_siirto = self.tekoaly.anna_siirto()
         self.kerro_tietokoneen_valinta()
+        self.talleta_ekan_siirto(self.ekan_siirto)
         return self.tokan_alykas_siirto
         
     def kerro_tietokoneen_valinta(self):
         print(f"Tietokone valitsi: {self.tokan_alykas_siirto}")
+
+    def talleta_ekan_siirto(self,siirto):
+        self.tekoaly.aseta_siirto(siirto)
 
 
 
