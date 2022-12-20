24b3312e0e157aab9c7da6c61d2d773d431e8131
Esko Hakala <esko.hakala@hotmail.com>
2022-12-20 18:24:53 +0200

    Tekoaly luokan attribuuttien ja metodien parempi nimeäminen

--- viikko7/kivi-paperi-sakset/src/tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/tekoaly.py
@@ -9,30 +9,30 @@ class Siirto(Enum):
 
 class Muisti:
     def __init__(self):
-        self.siirto_indeksi=0
+        self.uuden_siirron_indeksi = 0
         self.siirrot = [Siirto.Kivi.value,Siirto.Paperi.value,Siirto.Sakset.value]
-        self.seuraava_indeksi = 0
+        self._siirron_indeksi = 0
 
-    def tallenna_seuraavan_siirron_indeksi(self,indeksi):
-        self.seuraava_indeksi = indeksi
+    def tallenna_siirron_indeksi(self,indeksi):
+        self._siirron_indeksi = indeksi
 
-    def anna_seuraavan_siirron_indeksi(self):
-        return self.seuraava_indeksi
+    def anna_edellisen_siirron_indeksi(self):
+        return self._siirron_indeksi
 
 class Tekoaly:
     def __init__(self):
         self._muisti = Muisti()
-        self._siirto_indeksi=0
+        self._siirto_indeksi = 0
         self.siirrot = [Siirto.Kivi.value,Siirto.Paperi.value,Siirto.Sakset.value]
 
 
     def anna_siirto(self):
-        self._siirto_indeksi = self._muisti.anna_seuraavan_siirron_indeksi()
-        self._siirto_indeksi += 1
-        self._siirto_indeksi = self._siirto_indeksi % 3
-        self._muisti.tallenna_seuraavan_siirron_indeksi(self._siirto_indeksi)
+        self.uuden_siirron_indeksi = self._muisti.anna_edellisen_siirron_indeksi()
+        self.uuden_siirron_indeksi += 1
+        self.uuden_siirron_indeksi = self.uuden_siirron_indeksi % 3
+        self._muisti.tallenna_siirron_indeksi(self.uuden_siirron_indeksi)
 
-        return self._muisti.siirrot[self._siirto_indeksi]
+        return self._muisti.siirrot[self.uuden_siirron_indeksi]
 
     def aseta_siirto(self, siirto):
         # ei tehdä mitään
